1eb475d2bcb44318b69c55aa25d021af0335fac3
synta-x <syntaxcdev@gmail.com>
2021-06-01 11:59:52 -0500

    trying new things to improve program

---
+++ ddwipe.py
@@ -0,0 +1,7 @@
+class dd:
+    def __init__(self,name):
+      self.name = name
+
+    def wipe(self,if,of,bs,):
+        pass
+
--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import time
 import sys
 import signal
 
+# from ddwipe import *
+
+gi.require_version('Gtk','3.0')
+
 from subprocess import Popen, PIPE
 from gi.repository import Gtk as gtk
 
-gi.require_version('Gtk','3.0')
+# tdd.wipe(1,2,3)
 
 
 class Main:
@@ -28,17 +32,27 @@ class Main:
         startWipe = self.builder.get_object('startWipe')
         startWipe.connect('clicked', self.startWipe)
 
+        stopWipe = self.builder.get_object('stopWipe')
+        stopWipe.connect('clicked', self.stopWipe)
+
         launchAbout = self.builder.get_object('aboutButton')
         launchAbout.connect('clicked', self.launchAbout)
 
         warning = self.builder.get_object('warning')
         warning.show()
 
+        launchOptions = self.builder.get_object('optionsButton')
+        launchOptions.connect('clicked', self.launchOptions)
+
     def launchAbout(self,widget):
         aboutwindow = self.builder.get_object('aboutdialog1')
         aboutwindow.run()
         aboutwindow.hide()
 
+    def launchOptions(self,widget):
+        optionsWindow = self.builder.get_object('optionsDialog')
+        optionsWindow.run()
+        optionsWindow.hide()
 
     def printText(self,widget):
         # gets the path specified
@@ -46,9 +60,24 @@ class Main:
         drivePath = selectDrive.get_text()
         print(drivePath)
 
+    def checkOutput(self):
+        viewer = self.builder.get_object('outputView')
+
+        while True:
+            output = self.process.stdout.readline()
+            if self.process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+
+        rc = self.process.poll()
+
     def startWipe(self,widget):
         selectDrive = self.builder.get_object('driveEntry')
         drivePath = selectDrive.get_text()
+
+        inputEntry = self.builder.get_object('inputEntry')
+        inputPath = inputEntry.get_text()
 
         blockSize = self.builder.get_object('blockSize').get_text()
 
@@ -57,49 +86,36 @@ class Main:
         print(command)
 
 
-        dd = ['dd', 'if=/tmp/test.txt', 'of='+drivePath,'bs='+blockSize,'status=progress']
-        # while dd.poll() is None:
-            # time.sleep(.3)
-            # dd.send_signal(signal.SIGUSR1)
-            # while 1:
-                # l = dd.stderr.readline()
-                # if 'records in' in l:
-                    # print l[:l.index('+')], 'records',
-                # if 'bytes' in l:
-                    # print l.strip(), '\r',
-                    # break
-        # print dd.stderr.read(),
+        dd = ['dd', 'if='+inputPath, 'of='+drivePath,'bs='+blockSize,'status=progress']
 
-        # process = subprocess.Popen(dd, stderr=subprocess.STDOUT)
+        self.process = subprocess.Popen(dd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-        # line = ''
+        self.checkOutput()
+
+    
+
         # while True:
-        #     out = process.stderr.read(1)
-        #     if out == '' and process.poll() != None:
-        #         break
-        #     if out != '':
-        #         s = out.decode("utf-8")
-        #         if s == '\r':
-        #             print(line)
-        #             ddOut = self.builder.get_object('ddOut')
-        #             ddOut.set_text(line)
-        #             line = ''
-        #         else:
-        #             line = line + s
+            # # out = subprocess.check_output(dd).decode(sys.stdout.encoding)
+            # out, err = process.communicate()
+            # ddOut = self.builder.get_object('ddOut')
+            # if out == None:
+                # pass
+            # else:
+                # outStr = str(out)
+                # ddOut.set_label('outStr')
+                # print(outStr)
 
-        while True:
-            out = subprocess.check_output(dd).decode(sys.stdout.encoding)
-            ddOut = self.builder.get_object('ddOut')
-            ddOut.set_label(out)
-            print(out)
+            # poll = process.poll()
+            # if poll is None:
+                # # process is alive
+                # pass
+            # else:
+                # # process has ended
+                # break
+    def stopWipe(self,widget):
+        self.process.kill()
 
-            poll = out.poll()
-            if poll is None:
-                # process is alive
-                pass
-            else:
-                # process has ended
-                break
+    
 
 
 
